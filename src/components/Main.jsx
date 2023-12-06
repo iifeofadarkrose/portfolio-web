d0d3803767137e4aabe08825373098ae0ec4f99f
iifeofadarkrose <122921193+iifeofadarkrose@users.noreply.github.com>
2023-12-06 23:43:26 +0100

    upgrade

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,8 +12,8 @@ const Main = () => {
       />
       <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
         <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
-          <h1 className="sm:text-5xl text-4xl text-gray-800">I'm Viacheslav Babenko</h1>
-          <h2 className="flex center sm:text-3xl text-2xl pt-4 text-gray-800">
+          <h1 className="smm:text-3xl sm:text-5xl text-4xl text-gray-800">I'm Viacheslav Babenko</h1>
+          <h2 className="flex center smm:text-2xl sm:text-3xl text-2xl pt-4 text-gray-800">
             I'm a
             <TypeAnimation
               sequence={[
@@ -23,8 +23,6 @@ const Main = () => {
                 2000,
                 "Tech Enthusiast",
                 2000,
-                // "We produce food for Chinchillas",
-                // 2000,
               ]}
               wrapper="span"
               speed={50}
--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -10,10 +10,10 @@ const Sidenav = () => {
 }
   return (
     <div>
-        <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
+        <AiOutlineMenu size={40} onClick={handleNav} className='absolute top-4 right-4 z-[99] border m-3 p-2 rounded-3xl border-[#001b5e] md:hidden'/>
         {
             nav ? (
-                <div className='fixed w-full h-screen gb-white/90 flex flex-col justify-center items-center z-20'>
+                <div className='fixed w-full h-screen bg-black/75 flex flex-col justify-center items-center z-20'>
                     <a onClick={handleNav} href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-800'>
                         <AiOutlineHome size={20}/>
                         <span className='pl-4 '>Home</span>

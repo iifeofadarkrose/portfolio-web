ecc581d934b7ef3424c6c223ce8f98e2af5f6c14
iifeofadarkrose <122921193+iifeofadarkrose@users.noreply.github.com>
2023-12-06 19:20:41 +0100

    SideNavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
-import './App.css'
+import Sidenav from './components/Sidenav'
 
 function App() {
   return (
     <div>
-     
+     <Sidenav/>
     </div>
   )
 }
--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,8 +1,68 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import {AiOutlineMenu, AiOutlineHome, AiOutlineProject, AiOutlineMail, AiOutlinePicture} from 'react-icons/ai'
+import {GrProjects} from'react-icons/gr'
+// import {BsPerson} from 'react-icons/bs'
 
 const Sidenav = () => {
+    const [nav,setNav] = useState(false) 
+    const handleNav = () =>{
+    setNav(!nav);
+}
   return (
-    <div>Sidenav</div>
+    <div>
+        <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
+        {
+            nav ? (
+                <div className='fixed w-full h-screen gb-white/90 flex flex-col justify-center items-center z-20'>
+                    <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-800'>
+                        <AiOutlineHome size={20}/>
+                        <span className='pl-4 '>Home</span>
+                    </a>
+                    <a href="#work" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-800'>
+                        <GrProjects size={20}/>
+                        <span className='pl-4 '>Work</span>
+                    </a>
+                    <a href="#projects" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-800'>
+                        <AiOutlineProject size={20}/>
+                        <span className='pl-4 '>Projects</span>
+                    </a>
+                    <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-800'>
+                        <AiOutlinePicture size={20}/>
+                        <span className='pl-4 '>Resume</span>
+                    </a>
+                    <a href="#contact" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-800'>
+                        <AiOutlineMail size={20}/>
+                        <span className='pl-4 '>Contact</span>
+                    </a>
+                </div>
+            )
+            :(
+                ''
+            )
+        }
+
+        <div className='md:block hidden fixed top-[25%] z-10'>
+            <div className='flex flex-col'>
+                <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <AiOutlineHome size={20}/>
+                </a>
+                <a href="#work" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <GrProjects size={20}/>
+                </a>
+                <a href="#projects" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <AiOutlineProject size={20}/>
+                </a>
+                <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <AiOutlinePicture size={20}/>
+                </a>
+                <a href="#contact" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <AiOutlineMail size={20}/>
+                </a>
+            </div>
+        </div>
+
+
+    </div>
   )
 }
 
